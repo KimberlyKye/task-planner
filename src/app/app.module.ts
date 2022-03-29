6fc0e7b7b8b9bbe63350a743822895947f832d85
Anastasia Sharova <anastasia.sharova@arcadia.spb.ru>
2022-03-29 23:41:06 +0300

    Create header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CalenderComponent } from './calender/calender.component';
 import { SelectorComponent } from './selector/selector.component';
 import { OrganizerComponent } from './organizer/organizer.component';
+import { MomentPipe } from './shared/moment.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalenderComponent,
     SelectorComponent,
-    OrganizerComponent
+    OrganizerComponent,
+    MomentPipe
   ],
   imports: [
     BrowserModule,
